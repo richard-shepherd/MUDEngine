319a476ba88935b214c6273b20be0049df23eab0
richard-shepherd <richard.s.shepherd@gmail.com>
2021-08-07 11:12:13 +0100

    MUDEngine initial coding.

--- Tests/ContainerTests.cs
+++ Tests/ContainerTests.cs
@@ -45,10 +45,39 @@ namespace Tests
             // We create a small-bag...
             var smallBag = m_objectFactory.createObjectAs<Container>("small-bag");
 
-            // We load two apples...
+            // We apples and add them to the bag, checking that each addition succeeds...
+            var apple = m_objectFactory.createObjectAs<Container>("apple");
+            var result = smallBag.add(apple);
+            Assert.AreEqual(ActionResult.StatusEnum.SUCCEDED, result.Status);
+            Assert.AreEqual(1, smallBag.ItemCount);
 
-            // We add two apples
+            apple = m_objectFactory.createObjectAs<Container>("apple");
+            result = smallBag.add(apple);
+            Assert.AreEqual(ActionResult.StatusEnum.SUCCEDED, result.Status);
+            Assert.AreEqual(2, smallBag.ItemCount);
 
+            apple = m_objectFactory.createObjectAs<Container>("apple");
+            result = smallBag.add(apple);
+            Assert.AreEqual(ActionResult.StatusEnum.SUCCEDED, result.Status);
+            Assert.AreEqual(3, smallBag.ItemCount);
+
+            apple = m_objectFactory.createObjectAs<Container>("apple");
+            result = smallBag.add(apple);
+            Assert.AreEqual(ActionResult.StatusEnum.SUCCEDED, result.Status);
+            Assert.AreEqual(4, smallBag.ItemCount);
+
+            apple = m_objectFactory.createObjectAs<Container>("apple");
+            result = smallBag.add(apple);
+            Assert.AreEqual(ActionResult.StatusEnum.SUCCEDED, result.Status);
+            Assert.AreEqual(5, smallBag.ItemCount);
+
+            // We add another apple. This addition should fail as the small-bag can 
+            // only hold five items...
+            apple = m_objectFactory.createObjectAs<Container>("apple");
+            result = smallBag.add(apple);
+            Assert.AreEqual(ActionResult.StatusEnum.FAILED, result.Status);
+            Assert.AreNotEqual("", result.Description);
+            Assert.AreEqual(5, smallBag.ItemCount);
         }
     }
 }
